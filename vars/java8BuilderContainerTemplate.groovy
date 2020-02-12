21737c529ed6606a4d990937e330fde26bf00040
Sean Dawson <spdawson@agiledigital.com.au>
2020-02-12 12:01:15 +1100

    Add tag to image reference

--- vars/java8BuilderContainerTemplate.groovy
+++ vars/java8BuilderContainerTemplate.groovy
@@ -2,7 +2,7 @@ def call(String path) {
   return [
     containerTemplate(
       name: 'java8-builder',
-      image: "agiledigital/java8-builder",
+      image: 'agiledigital/java8-builder:latest',
       alwaysPullImage: true,
       command: 'cat',
       ttyEnabled: true
