e46e26031c66eb41fc7eade98b3b22ca1a0c6617
Sean Dawson <spdawson@agiledigital.com.au>
2020-02-12 11:56:44 +1100

    Fix image reference

--- vars/java8BuilderContainerTemplate.groovy
+++ vars/java8BuilderContainerTemplate.groovy
@@ -2,7 +2,7 @@ def call(String path) {
   return [
     containerTemplate(
       name: 'java8-builder',
-      image: "${path}/builders/java8-builder",
+      image: "agiledigital/java8-builder",
       alwaysPullImage: true,
       command: 'cat',
       ttyEnabled: true
